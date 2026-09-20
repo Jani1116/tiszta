83b201c7e6b9dfa53b3c5ac737b26b98160df0fc
Jani <pikypapa@gmail.com>
2026-09-20 16:58:07 +0200

    Update service worker cache management
    Updated cache name and added activation event to manage old caches.

--- sw.js
+++ sw.js
@@ -1,8 +1,9 @@
-const CACHE_NAME = 'naptar-v2';
+const CACHE_NAME = 'naptar-v3';
 const assets = [
-  'index.html',
-  'manifest.json',
-  'kep.png'
+  './',
+  './index.html',
+  './manifest.json',
+  './kep.png'
 ];
 
 self.addEventListener('install', e => {
@@ -11,6 +12,22 @@ self.addEventListener('install', e => {
       return cache.addAll(assets);
     })
   );
+  self.skipWaiting(); // Azonnal vegye át az irányítást
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key); // Törli a régi cache-eket
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
